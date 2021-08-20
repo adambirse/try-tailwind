8a48ccf1d32e3c2d9174224ed1f4c86e27c137a9
Adam Birse <adambirse@gmail.com>
2021-08-20 08:02:51 +0100

    home page updated

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
 export default function Page() {
-  return <>My NextJS bootstrap project</>;
+  return <><h1>My NextJS bootstrap project</h1>
+  <p>A starter for 10 project to bootstrap nextJS development</p></>;
 }
