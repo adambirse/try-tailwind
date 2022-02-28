3e2a69dc05b9f9689017a141f7e451cf03594f64
Adam Birse <adambirse@gmail.com>
2022-02-28 15:31:21 +0000

    lint

--- README.md
+++ README.md
@@ -20,11 +20,10 @@ git push
 
 ## Setting up Jest with CSS modules
 
-Every time I setup a project I forget how to do this,  and waste more time than I should.
+Every time I setup a project I forget how to do this, and waste more time than I should.
 
 https://jestjs.io/docs/webpack
 
 This commit:
 
 https://github.com/adambirse/accessibility-test/commit/5f3a72965acd74018ce2030de680b5830fbb541b
-
--- setupTests.ts
+++ setupTests.ts
@@ -1,12 +1,11 @@
+import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
+import * as enzyme from "enzyme";
+import { JSDOM } from "jsdom";
 
-import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
-import * as enzyme from 'enzyme';
-import { JSDOM } from 'jsdom';
-
-const jsdom = new JSDOM('<!doctype html><html><body></body></html>', {
-    url: 'http://localhost/',
-  });
-  const { window } = jsdom;
+const jsdom = new JSDOM("<!doctype html><html><body></body></html>", {
+  url: "http://localhost/",
+});
+const { window } = jsdom;
 
 function copyProps(src, target) {
   Object.defineProperties(target, {
@@ -24,6 +23,5 @@ global.cancelAnimationFrame = function (id) {
   clearTimeout(id);
 };
 copyProps(window, global);
-
 
 enzyme.configure({ adapter: new Adapter() });
