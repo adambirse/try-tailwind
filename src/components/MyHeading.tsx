233c3a0fb8097a1dccfe5a17f8fd2a8007c8ab4c
Adam Birse <adambirse@gmail.com>
2022-03-01 08:47:43 +0000

    Center heading component.

--- src/components/MyHeading.tsx
+++ src/components/MyHeading.tsx
@@ -1,3 +1,7 @@
-export const MyHeading = ({children}) => {
-  return <h1 className="text-3xl font-bold underline">{children}</h1>;
+export const MyHeading = ({ children }) => {
+  return (
+    <div className="flex h-screen justify-center">
+      <h1 className="text-3xl font-bold underline">{children}</h1>
+    </div>
+  );
 };
