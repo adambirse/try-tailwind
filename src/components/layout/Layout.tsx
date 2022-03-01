3ee459b042eb74f10c6e4a433ff01d0c1981ee61
Adam Birse <adambirse@gmail.com>
2022-03-01 11:05:15 +0000

    added vertical space to layout

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -3,10 +3,10 @@ import { Navigation } from "../navigation/Navigation";
 
 export const Layout = ({ title = "Tailwind demo", children }) => {
   return (
-    <>
+    <div className="flex flex-col space-y-1">
       <Navigation></Navigation>
       <MyHeading>{title}</MyHeading>
       <div className="m-4">{children}</div>
-    </>
+    </div>
   );
 };
